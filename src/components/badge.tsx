fdb9807b8401d81b7508c1775cbb33bb05d995c5
miawithcode <cuttontail@gmail.com>
2024-10-04 18:32:59 +0800

    fix: build

--- src/components/badge.tsx
+++ src/components/badge.tsx
@@ -1,10 +1,16 @@
-// @ts-expect-error
-export default function Badge(props) {
+type BadgeProps = {
+  href: string;
+  children: React.ReactNode;
+};
+
+export default function Badge({ href, children }: BadgeProps) {
   return (
     <a
-      {...props}
+      href={href}
       target="_blank"
       className="text-text inline-flex items-center rounded border border-border bg-neutral-50 p-1 text-sm leading-4 no-underline hover:bg-neutral-100 dark:bg-neutral-800"
-    />
+    >
+      {children}
+    </a>
   );
 }
