23248f8b67acc400584451ca6fc76657eeaae5bc
miawithcode <cuttontail@gmail.com>
2024-10-04 22:27:58 +0800

    feat: add noStore

--- src/lib/queries/vault.ts
+++ src/lib/queries/vault.ts
@@ -1,6 +1,11 @@
+'use server';
+
+import { unstable_noStore as noStore } from 'next/cache';
+
 import { db } from '@/db';
 import { resources } from '@/db/schema';
 
 export async function getResources() {
+  noStore();
   return await db.select().from(resources);
 }
