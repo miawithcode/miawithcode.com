2118ac45618ac6804381c0c1779cb5cb82034609
miawithcode <cuttontail@gmail.com>
2024-10-04 19:22:59 +0800

    feat: add vault description

--- src/app/vault/page.tsx
+++ src/app/vault/page.tsx
@@ -1,3 +1,13 @@
 export default function Page() {
-  return <div>Vault</div>;
+  return (
+    <>
+      <section>
+        <h1 className="text-2xl font-bold">Vault</h1>
+        <p className="mt-1 text-muted">
+          A selection of useful links, tools I curated for frontend developers
+          and designers to inspire, learn, and create.
+        </p>
+      </section>
+    </>
+  );
 }
