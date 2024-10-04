366fadcd0ba0c2d47063abf186081c9017062504
miawithcode <cuttontail@gmail.com>
2024-10-04 23:31:14 +0800

    feat: add order

--- src/lib/queries/vault.ts
+++ src/lib/queries/vault.ts
@@ -4,8 +4,9 @@ import { unstable_noStore as noStore } from 'next/cache';
 
 import { db } from '@/db';
 import { resources } from '@/db/schema';
+import { asc } from 'drizzle-orm';
 
 export async function getResources() {
   noStore();
-  return await db.select().from(resources);
+  return await db.select().from(resources).orderBy(asc(resources.name));
 }
